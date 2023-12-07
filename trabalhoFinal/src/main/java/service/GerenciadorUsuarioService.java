c54a96b5052468c51d1c076f658bcd06a36f8b16
gabriee7 <gabrielsouzades@gmail.com>
2023-12-06 23:25:47 -0300

    set ativo usuario condicionado ao usuario autenticado ser admin ou nao

--- trabalhoFinal/src/main/java/service/GerenciadorUsuarioService.java
+++ trabalhoFinal/src/main/java/service/GerenciadorUsuarioService.java
@@ -5,7 +5,6 @@
 package service;
 
 import com.pss.senha.validacao.ValidadorSenha;
-import java.util.ArrayList;
 import java.util.List;
 import model.Usuario;
 import persistence.Factory.DAOFactoryService;
@@ -18,15 +17,18 @@ import persistence.IUsuarioDAO;
 public class GerenciadorUsuarioService {
     private IUsuarioDAO usuarioDAO;
     private DAOFactoryService factoryService;
+    private Usuario usuarioAutenticado;
 
     public GerenciadorUsuarioService() {
         this.factoryService = new DAOFactoryService();
         this.usuarioDAO = factoryService.getUsuarioDAO();
+        this.usuarioAutenticado = new AutenticacaoService().getUsuarioAutenticado();
     }
    
     public void inserir(String nome, String senha){
         ValidadorSenha validador = new ValidadorSenha();
         List<String> resultValida = validador.validar(senha);
+        
         if(!resultValida.isEmpty()){
             throw new RuntimeException("Erro: \n" + resultValida.toString().replace(";", "\n").replace(",", "").replace("[", "").replace("]", ""));
         }
@@ -35,10 +37,13 @@ public class GerenciadorUsuarioService {
         if(listarTodos().isEmpty()){
            usuario.setTipo("admin");
            usuario.setAtivo(true);
-        }else { // falta implementar set Ativo ou nao
-           usuario.setTipo("padrao");
+        }else if("admin".equalsIgnoreCase(usuarioAutenticado.getTipo())){
+            usuario.setAtivo(true);
+            usuario.setTipo("padrao");
+        }else{
+            usuario.setAtivo(false);
+            usuario.setTipo("padrao");
         }
-        System.out.println(usuario);
         usuarioDAO.criar(usuario);
     }
    
